c629aa05e423da56225af98d682eb33a584051de
MOHAMED-MAGDY <154827913+szdfgxch@users.noreply.github.com>
2025-12-11 21:26:20 +0200

    Add files via upload

--- import threading # Importing the threadi.py
+++ import threading # Importing the threadi.py
@@ -1,26 +1,22 @@
-import threading  
-import time  
-def print_1():
-    print("Starting of a thread:", threading.current_thread().name)
-    time.sleep(0.02)  
-    print("Finishing of a thread:", threading.current_thread().name)
-
-def print_2():
-    print("Starting of a thread 2:", threading.current_thread().name)
-    print("Finishing of a thread 2 :", 
-threading.current_thread().name)
-
-a = threading.Thread(target=print_1, name="Thread-1")
-b = threading.Thread(target=print_2, name="Thread-2")
-
-a.start()
-b.start()
-Starting of a thread: Thread-1
-Starting of a thread 2: Thread-2
-Finishing of a thread 2 : Thread-2
-Finishing of a thread: Thread-1
-
-a.join()  
-b.join() 
-print("Main thread: All threads have completed execution")
-Main thread: All threads have completed execution
+import threading
+import time
+In [ ]:
+In [ ]:
+In [ ]:
+In [ ]:
+examinationroom = threading.Semaphore(1)
+def enter_examination(num):
+global examinationroom
+print(f"patient {num} is waiting")
+examinationroom.acquire()
+print(f"patient {num} is in roon")
+time.sleep(2)
+print(f"patient {num} ")
+examinationroom.release()
+patients = []
+for i in range(10):
+patient_thread = threading.Thread(target=enter_examination, args=(i,))
+patients.append(patient_thread)
+patient_thread.start()
+for patient_thread in patients:
+patient_thread.join()
